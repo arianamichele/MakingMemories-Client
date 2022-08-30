3c16a66061047f53566bbfddd82803399c2d350f
YOUR-USERNAME <arianamichelebooking@gmail.com>
2022-08-30 00:21:14 -0700

    pagination feature added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => (
-    <BrowserRouter>
-        <Container maxwidth="lg">
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/auth" exact component={Auth} />
-            </Switch>
-        </Container>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Container maxWidth="xl">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/posts/pages/:page" exact component={Home} />
+        <Route path="/search/:searchQuery" exact component={Home} />
+        <Route path="/posts/:id" exact component={PostDetails} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
 );
-
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,34 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, AppBar, TextField, Button, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
-import { getPosts } from '../../actions/posts';
+import { useHistory } from 'react-router-dom';
+import { getPosts, getPostsBySearch } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination';
+import useStyles from './styles';
 
-const Home = () => {
-    const [currentId, setCurrentId] = useState(0);
-    const dispatch = useDispatch();
+const Home = ({ match }) => {
+  const { page, searchQuery } = match.params;
+  const classes = useStyles();
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
 
-    return (
-        <Grow in>
-            <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId} />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form currentId={currentId} setCurrentId={setCurrentId} />
-                    </Grid>
-                </Grid>
-            </Container>
-        </Grow>
-    )
-}
+  const [search, setSearch] = useState('');
+  const history = useHistory();
 
+  useEffect(() => {
+    dispatch(getPosts(page));
+  }, [currentId, dispatch, page]);
+
+  const searchPost = () => {
+    if (search.trim()) {
+      dispatch(getPostsBySearch(search));
+      history.push(`/search/${search}`);
+    } else {
+      history.push('/');
+    }
+  };
+
+  return (
+    <Grow in>
+      <Container maxWidth="xl">
+        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={9}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
+              <TextField name="search" variant="outlined" label="Search Memories" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
+              <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+            </AppBar>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper className={classes.pagination}>
+              {!searchQuery && <Pagination page={page} />}
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
 
 export default Home;
