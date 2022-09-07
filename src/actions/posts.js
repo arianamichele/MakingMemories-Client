1b4fbe256a463aedeb8abf0734bd3af7b2bd20e9
YOUR-USERNAME <arianamichelebooking@gmail.com>
2022-09-07 06:52:45 -0700

    search working, cleaned up files:

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,11 +1,25 @@
-import { FETCH_BY_SEARCH, FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_BY_SEARCH, FETCH_POST, FETCH_ALL, START_LOADING, END_LOADING, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
-export const getPosts = () => async (dispatch) => {
+export const getPost = (id) => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    dispatch({ type: START_LOADING })
+    const { data } = await api.fetchPost(id);
+
+    dispatch({ type: FETCH_POST, payload: data });
+    dispatch({ type: END_LOADING })
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPosts = (page) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING })
+    const { data } = await api.fetchPosts(page);
 
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: END_LOADING })
   } catch (error) {
     console.log(error);
   }
@@ -13,17 +27,22 @@ export const getPosts = () => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
+      dispatch({type: START_LOADING});
         const { data: { data } } = await api.fetchPostsBySearch(searchQuery)
 
         dispatch({ type: FETCH_BY_SEARCH, payload: data })
+        dispatch({ type: END_LOADING })
     } catch (error) {
         console.log(error)        
     }
 }
 
-export const createPost = (post) => async (dispatch) => {
+export const createPost = (post, history) => async (dispatch) => {
   try {
+    dispatch({type: START_LOADING});
     const { data } = await api.createPost(post);
+
+    history.push(`/posts/${data._id}`)
 
     dispatch({ type: CREATE, payload: data });
   } catch (error) {
